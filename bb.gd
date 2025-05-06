dc23161a4a609300a1f1738c24ac15cedd62fc0e
Your Name <you@example.com>
2025-05-06 15:28:06 +0200

    Left Right Thrusters

--- bb.gd
+++ bb.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
+@export var fireIncrements = 0.1 # the shader increments 
+
+@export var angularVelocity = 0 # how fast is the ship turning
+@export var maxAngularVelocity = 0.05 # how fast the ship MAXIMALY turning
+
+
 var screen_size # Size of the game window.
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,27 +18,58 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO # The player's movement vector.
+	
+	var curFire = $FireRightThurster.material.get("shader_parameter/fire_intensity")
 	if Input.is_action_pressed("turn_right"):
-		self.rotate(0.1)
+		velocity.y -= 0.1
+		if (curFire<4):
+			curFire+=fireIncrements
+		if (angularVelocity<maxAngularVelocity):
+			angularVelocity += 0.001
+	else:
+		if (angularVelocity>0):
+			angularVelocity -= 0.001
+		if (curFire>0):
+			curFire-=fireIncrements
+	$FireRightThurster.material.set("shader_parameter/fire_intensity",curFire)
+	
+	
+	curFire = $FireLeftThurster.material.get("shader_parameter/fire_intensity")
 	if Input.is_action_pressed("turn_left"):
-		self.rotate(-0.1)
-	if Input.is_action_pressed("acc"):
-		velocity.y -= 1
-		#$FireThrusters.material("fire_intensity",3)
+		velocity.y -= 0.1
+		if (curFire<4):
+			curFire+=fireIncrements
+		if (angularVelocity>-maxAngularVelocity):
+			angularVelocity -= 0.001
+	else:
+		if (angularVelocity<0):
+			angularVelocity += 0.001
+		if (curFire>0):
+			curFire-=fireIncrements
+	$FireLeftThurster.material.set("shader_parameter/fire_intensity",curFire)
+
 		
-	if Input.is_action_pressed("break"):
-		velocity.y += 1
-		#$firethrusters.material("fire_intensity",0)
-		#test2 - remove this comment
+	curFire = $FireMainThrusters.material.get("shader_parameter/fire_intensity")
+	if Input.is_action_pressed("acc"):
+		velocity.y -= 1	
+		if (curFire<4):
+			curFire+=fireIncrements		
+	else:
+		if (curFire>0):
+			curFire-=fireIncrements
+	$FireMainThrusters.material.set("shader_parameter/fire_intensity",curFire)
+		
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	
+	self.rotate(angularVelocity)
 	velocity = velocity.rotated(self.rotation)
+	
 	position += velocity * delta
 	
 	position = position.clamp(Vector2.ZERO, screen_size)
-	$FireThrusters.material.set("shader_parameter/fire_intensity",3.0)
+
 	
 	
 	
